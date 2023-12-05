7ba2d1402673e5928af0042575b3da51b2de08b5
idinaloq <idinaloq@icloud.com>
2023-12-05 18:46:03 +0900

    FEAT: Add TableViewCell

--- ProjectManager/ProjectManager/RootViewController.swift
+++ ProjectManager/ProjectManager/RootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  ProjectManager
 //
 //  Created by idinaloq on 2023/12/04.
@@ -7,12 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
 
 }
 
---
+++ ProjectManager/View/TableViewCell.swift
@@ -0,0 +1,84 @@
+//
+//  TableViewCell.swift
+//  ProjectManager
+//
+//  Created by idinaloq on 2023/12/05.
+//
+
+import UIKit
+
+final class TableViewCell: UITableViewCell {
+    private let titleLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 1
+        
+        return label
+    }()
+    
+    private let bodyLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 3
+        
+        return label
+    }()
+    
+    private let deadLineLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 3
+        
+        return label
+    }()
+    
+    private let titleStackView: UIStackView = {
+        let stackView: UIStackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        
+        return stackView
+    }()
+    
+    private let bodyStackView: UIStackView = {
+        let stackView: UIStackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        
+        return stackView
+    }()
+    
+    private let deadLineStackView: UIStackView = {
+        let stackView: UIStackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        
+        return stackView
+    }()
+    
+    private func configureUI() {
+        titleStackView.addArrangedSubview(titleLabel)
+        bodyStackView.addArrangedSubview(bodyLabel)
+        deadLineStackView.addArrangedSubview(deadLineLabel)
+        contentView.addSubview(titleStackView)
+        contentView.addSubview(bodyStackView)
+        contentView.addSubview(deadLineStackView)
+    }
+    
+    private func configureLayout() {
+        NSLayoutConstraint.activate([
+            titleStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            titleStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titleStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            titleStackView.bottomAnchor.constraint(equalTo: bodyStackView.topAnchor, constant: -8),
+            
+            bodyStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            bodyStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            bodyStackView.bottomAnchor.constraint(equalTo: deadLineStackView.topAnchor, constant: -8),
+            
+            deadLineStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            deadLineStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            deadLineStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+}
